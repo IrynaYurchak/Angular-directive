9f3460ae34535d9d627388745dc81fc5e0b6c70c
IrynaYurchak <yurchak.iryna2017@gmail.com>
2024-07-03 22:23:43 +0300

    Angular directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,64 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lessons2';
+  public newLogin!: string;
+  public newPassword!: string;
+  public newEmail!: string;
+  public users: { login: string, password: string, email: string }[] = [];
+  public editStatus = false;
+  public editIndex!: number;
+
+  add(): void {
+    const newUser = {
+      login: this.newLogin,
+      password: this.newPassword,
+      email: this.newEmail
+    };
+    this.users.push(newUser);
+    this.clearForm();
+  }
+
+  clearForm(): void {
+    this.newLogin = '';
+    this.newPassword = '';
+    this.newEmail = '';
+    this.editStatus = false;
+    this.editIndex = -1;
+  }
+
+  deleteUser(index: number): void {
+    if (index >= 0 && index < this.users.length) {
+      this.users.splice(index, 1);
+      
+    }
+  }
+
+  editUser(index: number): void {
+    const user = this.users[index];
+    if (user) {
+      this.newLogin = user.login;
+      this.newPassword = user.password;
+      this.newEmail = user.email;
+      this.editIndex = index;
+      this.editStatus = true;
+    }
+  }
+
+  updateUser(): void {
+    if (this.editIndex >= 0 && this.editIndex < this.users.length) {
+      this.users[this.editIndex].login = this.newLogin;
+      this.users[this.editIndex].password = this.newPassword;
+      this.users[this.editIndex].email = this.newEmail;
+      this.clearForm();
+    }
+  }
 }
